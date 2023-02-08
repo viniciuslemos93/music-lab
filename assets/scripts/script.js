49b9f831915e9ecd7402def5e738627daec9630d
Vinicius Lemos <vinycius1993@gmail.com>
2023-02-08 15:48:49 -0300

    implementando a função map para iterar o resultado

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,16 @@
-function fetchApiData () {
-    console.log('Chamando a função...')
+function fetchApiData () {    
     const url = 'https://api.deezer.com/user/2529'
-    fetch(url)
+    fetch(url)    
         .then(function(response) {
             return response.json();
         })
         .then(function(data){
             console.log(data)
+                //selecionar o elemento
+            data.map((item) => {
+                //criar o elemento
+            })
         })
+
+    
 }

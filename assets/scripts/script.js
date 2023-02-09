8b0f1fdf6579d38fdb81a47c829a398f987ea6c5
Vinicius Lemos <vinycius1993@gmail.com>
2023-02-09 15:31:18 -0300

    Selecionando o input para a busca

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,18 @@
-function fetchApiData () {    
-    const url = 'https://api.deezer.com/user/2529'
-    fetch(url)    
+function buscarArtista () {
+    var nomeArtista = document.getElementById('artista').value
+    console.log(nomeArtista)
+    const url = `https://api.deezer.com/artist/name/${nomeArtista}`
+    //const url = 'https://api.deezer.com/artist/27'
+    fetch(url)
         .then(function(response) {
             return response.json();
         })
         .then(function(data){
-            console.log(data)
+            console.log(data.name)
                 //selecionar o elemento
             data.map((item) => {
                 //criar o elemento
+                
             })
         })
 

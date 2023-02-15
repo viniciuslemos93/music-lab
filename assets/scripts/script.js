4445673f61e851c55ac510f5a5e83fc213fc8d06
Vinicius Lemos <vinycius1993@gmail.com>
2023-02-15 14:16:56 -0300

    feat: Mostrando os dados de retorno da API na tela

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -19,8 +19,13 @@ function createArtistInterface (data) {
     for (let chave in data) {
         // A variável "chave" recebe o valor de cada uma das chaves do objeto em questão
         console.log('A chave atual é: ', chave)
-    
+        //Criando o elemento
+        const div = document.createElement('div')
+        div.textContent = `${chave}: ${data[chave]}`
+
+        body.appendChild(div)
         
-}
+    }
 
 }
+fetchMusicData('27')
